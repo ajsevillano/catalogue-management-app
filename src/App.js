33ad081b3ed0150e258fd38d9f60b0d0e10fa1e6
Sevi <correosevi@gmail.com>
2021-02-10 11:19:38 +0000

    Add header component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.scss';
+import Header from './components/header/Header';
 
 function App() {
   return (
     <div className="App">
-      <h1>Ready</h1>
+      <Header />
     </div>
   );
 }
