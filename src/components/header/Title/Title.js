d12a97aff8d5152ba2a72096fbfe682cc8f64c27
Sevi <correosevi@gmail.com>
2021-02-10 12:04:08 +0000

    Add styles to header components

--- src/components/header/Title/Title.js
+++ src/components/header/Title/Title.js
@@ -1,7 +1,11 @@
 import React from 'react';
 
 function Title() {
-  return <div className="title">Products</div>;
+  return (
+    <div className="title">
+      <h1>Products</h1>
+    </div>
+  );
 }
 
 export default Title;
