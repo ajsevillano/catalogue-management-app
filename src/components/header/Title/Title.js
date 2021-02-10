7c3e795ab63abd4ee708818cb44abf19aa025d6b
Sevi <correosevi@gmail.com>
2021-02-10 20:16:41 +0000

    Create a styled button on the Title component

--- src/components/header/Title/Title.js
+++ src/components/header/Title/Title.js
@@ -3,7 +3,8 @@ import React from 'react';
 function Title() {
   return (
     <div className="title">
-      <h1>Products</h1>
+      <h1>Productos</h1>
+      <button>Descargar</button>
     </div>
   );
 }
