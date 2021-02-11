c1bdc50ea3d4df1de7af5be601952cb7cbce8b55
Sevi <correosevi@gmail.com>
2021-02-11 14:54:24 +0000

    add icon after the searchbar

--- src/components/Searchbar/Searchbar.js
+++ src/components/Searchbar/Searchbar.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEllipsisV } from '@fortawesome/free-solid-svg-icons';
 
 function Searchbar() {
   return (
     <div className="search-bar-container">
-      <input type="text" placeholder="Buscar productos..." />
+      <div className="search-bar">
+        <input type="text" placeholder="Buscar productos..." />
+        <FontAwesomeIcon
+          className="icon-faEllipsis"
+          icon={faEllipsisV}
+          size="lg"
+        />
+      </div>
     </div>
   );
 }
