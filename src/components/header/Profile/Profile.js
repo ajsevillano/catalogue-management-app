83755ad1e9e46c7d8e9f38c7ac0e4ffee7c828c2
Sevi <correosevi@gmail.com>
2021-02-12 12:55:39 +0000

    Add bell icon on profile bar

--- src/components/header/Profile/Profile.js
+++ src/components/header/Profile/Profile.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBell } from '@fortawesome/free-solid-svg-icons';
 import Profilepicture from '../../../assets/img/profile.jpg';
 
 function Profile() {
   return (
     <div className="profile">
       <p>Gestión 3.0</p>
-
+      <FontAwesomeIcon className="bell-icon" icon={faBell} size="lg" />
       <img src={Profilepicture} alt="Profile picture" />
     </div>
   );
