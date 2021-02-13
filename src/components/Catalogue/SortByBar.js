0781715de88d9485416d9b8832ac90d726d29db4
Sevi <correosevi@gmail.com>
2021-02-13 10:36:18 +0000

    Add styles to Sortby component

--- src/components/Catalogue/SortByBar.js
+++ src/components/Catalogue/SortByBar.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faSort } from '@fortawesome/free-solid-svg-icons';
+import { faSort, faCog } from '@fortawesome/free-solid-svg-icons';
 
 function SortByBar() {
   return (
     <div className="sort-bar">
-      <p>
-        Ordenar por <span>MÁS RECIENTES</span>
+      <p>Mostrando todos los productos</p>
+      <p className="sort-order">
+        <FontAwesomeIcon className="faCog" icon={faCog} size="1x" />
+        ordenar por <span>MÁS RECIENTES</span>
         <FontAwesomeIcon className="sort" icon={faSort} size="sm" />
       </p>
     </div>
