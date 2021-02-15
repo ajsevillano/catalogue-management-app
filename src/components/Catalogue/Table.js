b898975bcb8308a8a9a86573a5afd3ab908d4c50
Sevi <correosevi@gmail.com>
2021-02-15 17:04:06 +0000

    Categories now come from an state

--- src/components/Catalogue/Table.js
+++ src/components/Catalogue/Table.js
@@ -5,19 +5,23 @@ import Menubar from '../Menubar/Menubar';
 import SortBar from './SortByBar';
 
 function Table({ fetchUrl, setFetchUrl, productsData }) {
-  const categories = [
-    'Todos',
-    'Vinos',
-    'Cervezas',
-    'Refrescos',
-    'Lacteos',
-    'Aguas',
-    'Otros',
-  ];
+  const [categories, setCategories] = useState([
+    { name: 'Todos', isActive: 'si' },
+    { name: 'Vinos', isActive: 'si' },
+    { name: 'Cervezas', isActive: 'si' },
+    { name: 'Refrescos', isActive: 'si' },
+    { name: 'Lacteos', isActive: 'si' },
+    { name: 'Aguas', isActive: 'si' },
+    { name: 'Otros', isActive: 'si' },
+  ]);
 
   return (
     <div id="table" className="wrapper">
-      <Menubar categories={categories} setFetchUrl={setFetchUrl} />
+      <Menubar
+        categories={categories}
+        setCategories={setCategories}
+        setFetchUrl={setFetchUrl}
+      />
       <SortBar />
       <div className="table-catalogue">
         <div className="table-header">
--- src/components/Menubar/Menubar.js
+++ src/components/Menubar/Menubar.js
@@ -1,21 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-function Menubar({ categories, setFetchUrl }) {
+function Menubar({ categories, setCategories, setFetchUrl }) {
+  const [activeLink, setActivelink] = useState('');
   const MenuSelectHandler = (category) => {
-    category == 'Todos'
+    //Because the API doesn't return a category=All,
+    // I use this conditional to get the righ URL for showing all.
+    category.name == 'Todos'
       ? setFetchUrl(`http://api.uniondistribuidora.com/products`)
       : setFetchUrl(
-          `http://api.uniondistribuidora.com/products?category=${category}`
+          `http://api.uniondistribuidora.com/products?category=${category.name}`
         );
+
+    console.log(categories);
   };
 
   return (
     <div className="menubar">
       <nav>
         {categories &&
-          categories.map((category) => (
-            <p key={category} onClick={() => MenuSelectHandler(category)}>
-              {category}
+          categories.map((category, index) => (
+            <p
+              className={category.isActive}
+              key={index}
+              onClick={() => MenuSelectHandler(category)}
+            >
+              {category.name}
             </p>
           ))}
       </nav>
