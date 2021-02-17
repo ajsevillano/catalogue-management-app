71fbd3e0f56e7bf7fbbcbc500a23dc9472900ea7
Sevi <correosevi@gmail.com>
2021-02-17 12:28:57 +0000

    Create,style and refactory secondary button

--- src/components/buttons/ButtonSecundary.jsx
+++ src/components/buttons/ButtonSecundary.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-export default function ButtonPrimary({ icon, size, text }) {
+export default function ButtonSecundary({ icon, size, text }) {
   return (
-    <div className="Button-primary">
+    <div className="Button-secundary">
       <button>
         <FontAwesomeIcon icon={icon} size={size} /> {text}
       </button>
--- src/components/header/Title/Title.js
+++ src/components/header/Title/Title.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Button from '../../buttons/ButtonSecundary';
+import { faDownload } from '@fortawesome/free-solid-svg-icons';
 
 function Title() {
   return (
     <div className="title">
       <h1>Productos</h1>
-      <button tabIndex="1">Descargar</button>
+      <Button icon={faDownload} size={'sm'} text={'Descargar'} />
     </div>
   );
 }
