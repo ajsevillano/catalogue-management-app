c48994c0fe5945deb31decf3d86f1e55d6e4b761
Sevi <correosevi@gmail.com>
2021-02-20 13:20:41 +0000

    Create The structure of UpdatePage product

--- src/components/UpdateProduct/Form.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Form = () => {
-  return <div>Hola que ase</div>;
-};
-
-export default Form;
--- src/pages/UpdateProduct.js
+++ src/pages/UpdateProduct.js
@@ -1,16 +1,21 @@
 import React from 'react';
 
 //Components
-import Form from '../components/UpdateProduct/Form';
 
 const UpdateProduct = ({ match }) => {
   console.log(match);
   return (
     <>
       <section id="main" className="wrapper">
+        <div className="update-product-container">
+          <div className="update-product-side-menu">Lateral</div>
+          <div className="update-product-main-content">
+            <h1>Información general</h1>
+            <p>Productos / Vinos</p>
+          </div>
+        </div>
         <h1>{match.params.id}</h1>
       </section>
-      <Form />
     </>
   );
 };
