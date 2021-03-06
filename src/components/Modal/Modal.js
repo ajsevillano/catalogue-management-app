adef2ff38525c688c9fb7e73dd9b13384d072946
Sevi <correosevi@gmail.com>
2021-03-06 16:07:02 +0000

    Add animation to modal loading

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import ModalPicture from '../../assets/img/add-product.jpg';
 
 const Modal = ({ content, modalOpen }) => {
   if (!modalOpen) {
     return null;
   }
+
   return (
     <div className="modal-container">
       <div className="modal-box">
--- src/pages/ProductsCatalog.js
+++ src/pages/ProductsCatalog.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import axios from 'axios';
 
 //Components
@@ -44,6 +44,7 @@ const ProductsCatalog = () => {
       return products;
     }, 250);
   }, [fetchUrl]);
+
   return (
     <>
       <Modal modalOpen={modalOpen} content={<AddNewProduct />} />
