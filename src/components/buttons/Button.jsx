1c4cc1e92d446b4d85c0c3bb554ac1407abfb1ee
Sevi <correosevi@gmail.com>
2021-03-07 11:59:14 +0000

    Fix information on  documentation to the button Component

--- src/components/buttons/Button.jsx
+++ src/components/buttons/Button.jsx
@@ -1,12 +1,11 @@
 /** Button UI Component, logic to be removed
- * @param {Object} icon A font awesome icon passed as faName
+ * @param {Object} icon A font awesome. Check https://fontawesome.com/icons for types.
  * @param {string} size Defines the font awesome size of the icon
  * @param {string} text Defines the text in the button
  * @param {state} buttonLoading Change the icon to a spinner while the update is executed
  * @param {string} button Defines the style of the button: 'primary' and 'secundary' allowed.
  * @param {string} inlineStyle Optional inline style
  * @param {function} handleOnClick Callback function, defines onclick button functionality
- *
  */
 
 import React from 'react';
