b5485b6a7d9394268eb01ad2d9ff1f1f8337f771
Sevi <correosevi@gmail.com>
2021-03-07 11:57:20 +0000

    Add documentation to the button Component

--- src/components/buttons/Button.jsx
+++ src/components/buttons/Button.jsx
@@ -1,3 +1,14 @@
+/** Button UI Component, logic to be removed
+ * @param {Object} icon A font awesome icon passed as faName
+ * @param {string} size Defines the font awesome size of the icon
+ * @param {string} text Defines the text in the button
+ * @param {state} buttonLoading Change the icon to a spinner while the update is executed
+ * @param {string} button Defines the style of the button: 'primary' and 'secundary' allowed.
+ * @param {string} inlineStyle Optional inline style
+ * @param {function} handleOnClick Callback function, defines onclick button functionality
+ *
+ */
+
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSpinner } from '@fortawesome/free-solid-svg-icons';
