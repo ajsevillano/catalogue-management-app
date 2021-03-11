b2864d5cb6d429731197361429e4bb2dd243e0d3
Sevi <correosevi@gmail.com>
2021-03-11 16:46:23 +0000

    Add a new animation for product not found

--- src/components/ProductsCatalogue/TableRownoresults.js
+++ src/components/ProductsCatalogue/TableRownoresults.js
@@ -3,16 +3,25 @@ import Noresults from '../../assets/img/noData.png';
 import Button from '../buttons/Button';
 import { faPlus } from '@fortawesome/free-solid-svg-icons';
 
+//Lottie animation helper and json import
+import LottieAnimation from '../../utils/Lottie';
+import * as notFound from '../../assets/animations/empty.json';
+
 function TableRownoresults() {
   return (
     <div className="row-noresults">
-      <img src={Noresults} alt="" />
+      <LottieAnimation animationName={notFound} height={120} width={120} />
       <h2>No se ha encontrado resultados</h2>
       <p>
-        Parece que esta categoría aun no tiene productos, ¿Te gustaría añadir
-        uno?
+        Parece que no se encuentra ningun producto ¿Te gustaría añadir uno
+        nuevo?
       </p>
-      <Button icon={faPlus} size={'sm'} text={'Añadir productos'} />
+      <Button
+        icon={faPlus}
+        size={'sm'}
+        text={'Añadir producto'}
+        button={'primary'}
+      />
     </div>
   );
 }
