0e43bedd451f85ee8a948f9f8516ac2e037959ed
Sevi <correosevi@gmail.com>
2021-03-13 15:57:42 +0000

    Add more mobile styles

--- src/components/ProductsCatalogue/TableRownoresults.js
+++ src/components/ProductsCatalogue/TableRownoresults.js
@@ -11,8 +11,16 @@ function TableRownoresults() {
     <div className="row-noresults">
       <LottieAnimation animationName={notFound} height={120} width={120} />
       <h2>No se ha encontrado resultados</h2>
-      <p>Parece que no se encuentra ningun producto ¿Te gustaría añadir uno nuevo?</p>
-      <Button icon={faPlus} size={'sm'} text={'Añadir producto'} button={'primary'} />
+      <p>
+        Parece que no se encuentra ningun producto ¿Te gustaría añadir uno
+        nuevo?
+      </p>
+      <Button
+        icon={faPlus}
+        size={'sm'}
+        text={'Añadir producto'}
+        button={'primary'}
+      />
     </div>
   );
 }
