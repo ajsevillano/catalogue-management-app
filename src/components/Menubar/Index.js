fa6f3ca6739585581ea12bd6097552ce4239a0cc
Sevi <correosevi@gmail.com>
2021-10-11 15:09:37 +0100

    Refactory menuBar component in preparation for Customers section

--- src/components/Menubar/Index.js
+++ src/components/Menubar/Index.js
@@ -4,41 +4,54 @@ const Menubar = ({
   categories,
   setCategories,
   setFetchUrl,
-  setLoading,
+
   setOrderText,
 }) => {
-  const MenuSelectHandler = (category) => {
-    setLoading(true);
-    setOrderText('Más recientes');
-    //Because the API doesn't return a category=All,
-    // I use this conditional to get the righ URL for showing all.
-    category.name == 'Todos'
-      ? setFetchUrl(`https://dev.ajsevillano.com/products`)
-      : setFetchUrl(
-          `https://dev.ajsevillano.com/products?category=${category.name}`
-        );
+  function fetchCategory(category) {
+    /* The api only return all the products/customers at the endpoint /products */
+    const allProducts = `https://dev.ajsevillano.com/products`;
+    const productsByCategory = `https://dev.ajsevillano.com/products?category=${category.name}`;
+    return category.name == 'Todos'
+      ? setFetchUrl(allProducts)
+      : setFetchUrl(productsByCategory);
+  }
 
-    setCategories(
-      categories.map((item) => {
-        return (item.isActive === true) & (category.name != item.name)
-          ? { ...item, isActive: false }
-          : item.name === category.name
-          ? { ...item, isActive: true }
-          : item;
-      })
-    );
-  };
+  function changeCategoryStatus(category) {
+    const { name, isActive } = category;
+    const checkIsActive = !isActive
+      ? 'el item no esta activo'
+      : 'el item está activo';
+
+    console.log(checkIsActive);
+  }
+
+  function MenuSelectHandler(category) {
+    changeCategoryStatus(category);
+    fetchCategory(category);
+  }
+  // const MenuSelectHandler = (category) => {
+
+  //   setOrderText('Más recientes');
+
+  //   setCategories(
+  //     categories.map((item) => {
+  //       return (item.isActive === true) & (category.name != item.name)
+  //         ? { ...item, isActive: false }
+  //         : item.name === category.name
+  //         ? { ...item, isActive: true }
+  //         : item;
+  //     })
+  //   );
+  // };
 
   return (
     <div className="menubar">
       <nav>
         {categories?.map((category, index) => (
           <p
-            className={category.isActive == true ? 'isActive' : ''}
+            className={category.isActive == true ? 'isActive' : undefined}
             key={index}
-            onClick={
-              category.isActive ? undefined : () => MenuSelectHandler(category)
-            }
+            onClick={() => MenuSelectHandler(category)}
           >
             {category.name}
           </p>
